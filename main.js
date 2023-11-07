fe761cceffd291fcac9ed82e592089f149632aa8
j-schall <schall.jan01@gmail.com>
2023-11-07 17:28:04 +0100

    Skills has been added

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var moreButton = document.getElementById("more-button");
-var details = document.getElementById("details");
+const moreButton = document.getElementById("more-button");
+const details = document.getElementById("details");
 
 let count = 0;
-moreButton.addEventListener("click", (e) => {
+moreButton.addEventListener("click", () => {
     if (count == 0) {
         details.style.display = "block";
         count++;
@@ -11,3 +11,34 @@ moreButton.addEventListener("click", (e) => {
         count--;
     }
 });
+
+const hiddenSection = document.querySelector('.first-entry-container');
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    const sectionPosition = hiddenSection.offsetTop;
+
+    if (scrollPosition >= sectionPosition - window.innerHeight / 2) {
+        hiddenSection.style.display = 'block';
+    }
+});
+
+function goToWikipediaPage(stackItem) {
+    let item = stackItem.textContent;
+    switch(item) {
+        case "Java":
+            window.open("https://en.wikipedia.org/wiki/Java_(programming_language)");
+            break;
+        case "HTML":
+            window.open("https://en.wikipedia.org/wiki/HTML");
+            break;
+        case "CSS":
+            window.open("https://en.wikipedia.org/wiki/CSS");
+            break;
+        case "SQL":
+            window.open("https://en.wikipedia.org/wiki/SQL");
+            break;
+        case "Arduino":
+            window.open("https://en.wikipedia.org/wiki/Arduino");
+            break;
+    }
+}
